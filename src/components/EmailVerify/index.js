5e3162e9b65a7cda4c8dee3470f4a04a1593ead7
Abhinav80555 <abhinavsekar1515@gmail.com>
2022-07-28 12:02:18 +0530

    final

--- src/components/EmailVerify/index.js
+++ src/components/EmailVerify/index.js
@@ -28,7 +28,7 @@ const EmailVerify = () => {
 		<Fragment>
 			{validUrl ? (
 				<div className={styles.container}>
-					<img src="https://github.com/cyber-wolve/AuthInMern/blob/Password-Reset-In-MERN/client/src/images/success.png?raw=true" alt="success_img" className={styles.success_img} />
+					<img src="https://as1.ftcdn.net/v2/jpg/02/53/70/76/1000_F_253707611_3goDKUC9Gaaf3I9Bz1txoaxPv3piNhFH.jpg" alt="success_img" className={styles.success_img} />
 					<h1>Email verified successfully</h1>
 					<Link to="/login">
 						<button className={styles.green_btn}>Login</button>
